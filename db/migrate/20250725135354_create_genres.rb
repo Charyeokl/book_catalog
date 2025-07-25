623c4596b6556fd519d603db4748f9817c9a2bcc
Cini Laki <cinilaki@icloud.com>
2025-07-25 08:59:18 -0500

    update to create_genres.db

--- db/migrate/20250725135354_create_genres.rb
+++ db/migrate/20250725135354_create_genres.rb
@@ -1,9 +1,9 @@
-class CreateGenres < ActiveRecord::Migration[7.2]
+class CreateGenres < ActiveRecord::Migration[7.0]
   def change
     create_table :genres do |t|
-      t.string :name
-
+      t.string :name, null: false
       t.timestamps
     end
+    add_index :genres, :name, unique: true
   end
 end
