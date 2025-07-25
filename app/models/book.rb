bb2f09bdaadfd921d6d9cb1b4984dcabb044f462
Cini Laki <cinilaki@icloud.com>
2025-07-25 09:04:24 -0500

    app models update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :publisher
+  has_many :book_genres, dependent: :destroy
+  has_many :genres, through: :book_genres
+  
+  validates :title, :isbn, :publication_year, presence: true
+  validates :isbn, uniqueness: true
+  validates :rating, numericality: { in: 0.0..5.0 }, allow_nil: true
+  validates :publication_year, numericality: { 
+    only_integer: true, 
+    greater_than: 1800, 
+    less_than_or_equal_to: Date.today.year 
+  }
 end
