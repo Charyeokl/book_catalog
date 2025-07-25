804b42ceb4bda3bfe53e206f9c5e499b3eaa2268
Cini Laki <cinilaki@icloud.com>
2025-07-25 09:05:55 -0500

    seeds.db integration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,49 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Clear existing data
+[BookGenre, Book, Genre, Author, Publisher].each(&:destroy_all)
+
+# Create publishers
+10.times do
+  Publisher.create!(
+    name: Faker::Book.unique.publisher,
+    location: Faker::Address.country
+  )
+end
+
+# Create authors
+50.times do
+  Author.create!(
+    name: Faker::Book.unique.author,
+    birth_year: rand(1800..1990),
+    bio: Faker::Lorem.paragraphs(number: 2).join("\n\n")
+  )
+end
+
+# Create genres
+genres = Faker::Book.genres.uniq.map do |name|
+  Genre.find_or_create_by!(name: name)
+end
+
+# Create books with associations
+200.times do
+  book = Book.create!(
+    title: Faker::Book.title,
+    isbn: Faker::Code.unique.isbn(base: 13),
+    publication_year: rand(1900..2023),
+    rating: rand(1.0..5.0).round(1),
+    page_count: rand(80..800),
+    description: Faker::Lorem.paragraphs(number: 3).join("\n\n"),
+    author: Author.all.sample,
+    publisher: Publisher.all.sample
+  )
+  
+  # Assign 1-3 random genres
+  book.genres << genres.sample(rand(1..3))
+end
+
+puts "Seeded #{Publisher.count} publishers"
+puts "Seeded #{Author.count} authors"
+puts "Seeded #{Genre.count} genres"
+puts "Seeded #{Book.count} books"
+puts "Seeded #{BookGenre.count} genre associations"
