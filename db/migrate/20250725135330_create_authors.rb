a89fcdb2fbfe716e30fefb16f5064c59c88edb5a
Cini Laki <cinilaki@icloud.com>
2025-07-25 08:57:16 -0500

    Database mitagation file create_authors update

--- db/migrate/20250725135330_create_authors.rb
+++ db/migrate/20250725135330_create_authors.rb
@@ -1,11 +1,11 @@
-class CreateAuthors < ActiveRecord::Migration[7.2]
+class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
     create_table :authors do |t|
-      t.string :name
+      t.string :name, null: false
       t.integer :birth_year
       t.text :bio
-
       t.timestamps
     end
+    add_index :authors, :name
   end
 end
